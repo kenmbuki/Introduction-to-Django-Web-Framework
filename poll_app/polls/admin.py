935203fe9b05482d2dcb4087519d54adc4f90ec9
Ken Mbuki <ken.mbuki@savannahinformatics.com>
2015-05-20 17:42:01 +0300

    Project Template
    Customizing the project template

--- poll_app/polls/admin.py
+++ poll_app/polls/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+       ('Questions',{'fields':['question_text']}),
+       ('Date Information', {'fields':['pub_date'],'classes':['collapse']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin),
+
