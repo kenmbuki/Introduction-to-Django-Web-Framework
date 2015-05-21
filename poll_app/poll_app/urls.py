2d596a09fd6cacc82a2184940e4b7b1683a2cf19
Ken Mbuki <ken.mbuki@savannahinformatics.com>
2015-05-21 10:23:59 +0300

    Views
    Done with views...fyuks....that was like 'brute-force' learning

--- poll_app/poll_app/urls.py
+++ poll_app/poll_app/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+
 ]
